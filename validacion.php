c27ef84282e71fbfdab6d52a153d783f9deb3c82
Luis <cuadraluis26@gmail.com>
2026-02-22 11:03:50 -0600

    pantalla final de login

---
+++ validacion.php
@@ -0,0 +1,26 @@
+<!--Confirmacion de login correcto-->
+<?php 
+    session_start();
+    if(!isset($_SESSION['usuario'])){
+        header("Location: index.php");
+        exit;
+    }
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Confirmacion</title>
+ </head>
+ <body>
+    <div class="container mt-4">
+        <div class="alert alert-info mt-3">
+            <h1>Login conseguido</h1>
+            <h4>Bienvenido, <?= $_SESSION['usuario'] ?></h4>
+        </div>
+    </div>
+ </body>
+ </html>
